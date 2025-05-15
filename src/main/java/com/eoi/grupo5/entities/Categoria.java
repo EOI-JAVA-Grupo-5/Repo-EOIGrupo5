ef7b3ffc900a6df3a10050d999caf8e8046e66c5
José Ángel García Pérez <73956458+JoseAngelGarciaPerez@users.noreply.github.com>
2025-05-15 19:20:11 +0200

    Creación de entidades Categoria, Producto y Supermercado

---
+++ src/main/java/com/eoi/grupo5/entities/Categoria.java
@@ -0,0 +1,25 @@
+package com.eoi.grupo5.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Categoria categoriaPadre;
+
+    private String nombre;
+}
