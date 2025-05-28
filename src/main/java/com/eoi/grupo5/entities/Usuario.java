31ef18544efb4cc66a96d523d2677cc7fbd3f097
José Ángel García Pérez <73956458+JoseAngelGarciaPerez@users.noreply.github.com>
2025-05-28 19:29:55 +0200

    Modelo de Usuario V1

--- src/main/java/com/eoi/grupo5/entities/Usuario.java
+++ src/main/java/com/eoi/grupo5/entities/Usuario.java
@@ -1,12 +1,62 @@
 package com.eoi.grupo5.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.*;
 
-@Data
+import java.io.Serial;
+import java.io.Serializable;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
-public class Usuario {
+@Table(name = "usuarios")
+public class Usuario implements Serializable {
+
+    /**
+     * SerialVersion
+     * */
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Atributos de Usuario
+     */
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @NonNull
+    private String nick;
+
+    @NonNull
+    private String password;
+
+    @NonNull
+    private String  correo;
+
+    private String  imagenURL;
+
+    private String  nombre;
+
+    private String  apellidos;
+
+    private String  telefono;
+
+    @NonNull
+    private Tipo tipo;
+
+
+    /**
+     * Inserción de enum "Tipo"
+     */
+
+    public static enum Tipo{
+        ADMIN, USER
+    }
+
 }
