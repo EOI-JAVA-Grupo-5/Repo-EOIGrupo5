f4909b5fb1931654c84b285ac8be550108472485
José Ángel García Pérez <73956458+JoseAngelGarciaPerez@users.noreply.github.com>
2025-05-29 18:55:17 +0200

    Subida de repo y servicio de Usuario

---
+++ src/main/java/com/eoi/grupo5/services/UsuarioService.java
@@ -0,0 +1,34 @@
+package com.eoi.grupo5.services;
+
+import com.eoi.grupo5.entities.Usuario;
+import com.eoi.grupo5.repositories.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public List<Usuario> findAll(){
+        return usuarioRepository.findAll();
+    }
+
+    public Optional<Usuario> findById(Long id){
+        return usuarioRepository.findById(id);
+    }
+
+    public Usuario save(Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
+
+    public void delete(Usuario usuario){
+        usuarioRepository.delete(usuario);
+    }
+}
